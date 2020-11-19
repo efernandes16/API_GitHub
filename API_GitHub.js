309d9c8ec054adba74eac936a34afaa83d1e2afc
efernandes16 <ericafernandesrj@yahoo.com.br>
2020-11-18 21:57:58 -0300

    Arquivo JS - API_GitHub alterado e adicionado index.html

--- API_GitHub.js
+++ API_GitHub.js
@@ -1,20 +1,71 @@
-let nomeUsuario = document.getElementById('nomeUsuario')
-let input = document.getElementById('entrada')
+let usuarioNaTela = document.getElementById('usuarioNaTela')
+let ipt = document.getElementById('input')
 let botao = document.getElementById('botao')
-botao.addEventListener("click", request)
+botao.addEventListener("click", requestUsuario)
+botao.addEventListener("click", requestRepositorio)
 
-function request() {
+let tituloRepositorios = document.getElementById('tituloRepositorios')
+let tituloLinks = document.getElementById('tituloLinks')
+let repositorios = document.getElementById('repositorios')
+// let linkRepositorios = document.getElementById('linkRepositorios')
+
+let body = document.querySelector("body")
+let linkRepositorios = document.createElement("a")
+body.appendChild(linkRepositorios)
+
+function requestUsuario() {
     let usuario = new XMLHttpRequest();
 
-    usuario.open("GET", `https://api.github.com/users/${input}`)
-    console.log(input)
-    // usuario.open("GET", `https://api.github.com/users/efernandes16`)
-
     usuario.addEventListener("load", function () {
-        let infoUsuario = JSON.parse(this.responseText)
+            if (this.status == 404){
+                usuarioNaTela.innerText = "Usuário não encontrado"
+            } else {
+                let infoUsuario = JSON.parse(this.responseText)
+                usuarioNaTela.innerText = infoUsuario.login
+            }
+     })
 
-        nomeUsuario.innerText = infoUsuario.login
-    })
-
+    usuario.open("GET", `https://api.github.com/users/${ipt.value}`, false)
     usuario.send()
+}
+
+// Descobrir uma forma de carregar o API do usuario e mostrar na tela o que falta
+
+function requestRepositorio() {
+    let repos = new XMLHttpRequest();
+
+    repos.addEventListener("load", function () {
+            if (this.status == 404){
+                repositorios.innerText = "Usuário não encontrado"
+            } else {
+                let infoRepos = JSON.parse(this.responseText);
+
+                let arrayDeRepos = []
+                for (let i = 0; i < infoRepos.length; i++) {
+                    arrayDeRepos.push("<li>" + infoRepos[i].name + "</li>")
+                }
+                tituloRepositorios.textContent = "Repositórios do usuário:"
+                repositorios.innerHTML = arrayDeRepos.join("")
+                
+            }
+     })
+
+     repos.addEventListener("load", function(){
+        if (this.status == 404){
+            repositorios.innerText = "Usuário não encontrado"
+        } else {
+            let infoRepos = JSON.parse(this.responseText);
+
+            let arrayDeRepos = []
+            for (let i = 0; i < infoRepos.length; i++) {
+                arrayDeRepos.push(`${infoRepos[i].clone_url}`).toString()
+            }
+            tituloLinks.textContent = "Link para os Repositórios do usuário:"
+            linkRepositorios.innerHTML = arrayDeRepos
+            
+        }
+     })
+
+    repos.open("GET", `https://api.github.com/users/${ipt.value}/repos`, false)
+    repos.send()
 }
