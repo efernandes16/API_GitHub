58d6998d8b8d4a1ddc90f6440b99c3e07f50f270
efernandes16 <ericafernandesrj@yahoo.com.br>
2020-11-19 12:03:31 -0300

    Correção do código JS do projeto

--- API_GitHub.js
+++ API_GitHub.js
@@ -7,11 +7,8 @@ botao.addEventListener("click", requestRepositorio)
 let tituloRepositorios = document.getElementById('tituloRepositorios')
 let tituloLinks = document.getElementById('tituloLinks')
 let repositorios = document.getElementById('repositorios')
-// let linkRepositorios = document.getElementById('linkRepositorios')
 
 let body = document.querySelector("body")
-let linkRepositorios = document.createElement("a")
-body.appendChild(linkRepositorios)
 
 function requestUsuario() {
     let usuario = new XMLHttpRequest();
@@ -21,7 +18,7 @@ function requestUsuario() {
                 usuarioNaTela.innerText = "Usuário não encontrado"
             } else {
                 let infoUsuario = JSON.parse(this.responseText)
-                usuarioNaTela.innerText = infoUsuario.login
+                usuarioNaTela.innerText = `User: ${infoUsuario.login}`
             }
      })
 
@@ -42,28 +39,12 @@ function requestRepositorio() {
 
                 let arrayDeRepos = []
                 for (let i = 0; i < infoRepos.length; i++) {
-                    arrayDeRepos.push("<li>" + infoRepos[i].name + "</li>")
+                    arrayDeRepos.push(`<li><a href=${infoRepos[i].clone_url}>  ${infoRepos[i].name} </a> </li>`)
                 }
                 tituloRepositorios.textContent = "Repositórios do usuário:"
                 repositorios.innerHTML = arrayDeRepos.join("")
                 
             }
-     })
-
-     repos.addEventListener("load", function(){
-        if (this.status == 404){
-            repositorios.innerText = "Usuário não encontrado"
-        } else {
-            let infoRepos = JSON.parse(this.responseText);
-
-            let arrayDeRepos = []
-            for (let i = 0; i < infoRepos.length; i++) {
-                arrayDeRepos.push(`${infoRepos[i].clone_url}`).toString()
-            }
-            tituloLinks.textContent = "Link para os Repositórios do usuário:"
-            linkRepositorios.innerHTML = arrayDeRepos
-            
-        }
      })
 
     repos.open("GET", `https://api.github.com/users/${ipt.value}/repos`, false)
